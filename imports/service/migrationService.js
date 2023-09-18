2ad1bfdc8af01978ee881ad2810f49b8d1d430f0
Tim Cooper <tjscooper@me.com>
2023-09-18 11:13:13 -0400

    fixes

--- imports/service/migrationService.js
+++ imports/service/migrationService.js
@@ -10,7 +10,6 @@ import { calculateLeaderboard } from './leaderboardService';
 import { ENTITY } from '../model/entities';
 
 // Constants
-const MIGRATION_VERSION = 1.0;
 const DEBUG = true;
 
 // Service
@@ -22,7 +21,7 @@ class MigrationService {
   // Methods
   async v_01_refreshLeaderboards_up() {
 
-    console.log('MIGRATION', 'v_01_refreshLeaderboards_up');
+    console.log('\n\nMIGRATION', 'v_01_refreshLeaderboards_up\n');
 
     try {
 
@@ -35,28 +34,44 @@ class MigrationService {
       if (leaderboardsBefore.length === 0) {
         console.log('02 - if empty, get weeks, sort by number');
         
-        const weeks = WeeksCollection.find({}, { number: 1 }).fetch();
-        console.log('weeks', weeks);
+        const weeks = WeeksCollection.find({}, { sort: { number: 1 } }).fetch();
+        
+        const taskTimer = ms => new Promise(res => setTimeout(res, ms));
+        const timerSetting = 30; // seconds
 
-        weeks.forEach(async (week, index) => {
-          let result = await calculateLeaderboard({ number: week.number, year: week.year }, true);
-          console.log(`03-${index} - insert leaderboard for week ${week.number} [${ result ? 'success' : 'fail' }]`);
-          result = null;
-        });
+        const performTasks = async () => { 
+          for (let taskNumber = 0; taskNumber < weeks.length; taskNumber++) {
+            console.log(`05 - performing task ${taskNumber + 1}`);
 
-        // Get leaderboards, verify weeks count is equal
-        console.log('04 - get leaderboards to verify');
-        const leaderboardsAfter = LeaderboardsCollection.find({}).fetch();
-        console.log('05 - found leaderboards', leaderboardsAfter.length);
-        if (leaderboardsAfter.length === weeks.length) {
-          console.log('06 - migration successful');
-          return true;
-        } else {
-          console.log('02 - operation failed, run down migration');
-          // Failed migration, run down migration
-          return this.v_01_refreshLeaderboards_down();
+            // Task logic
+            const { number, year } = weeks[taskNumber];
+            const result = await calculateLeaderboard({ number, year }, true); // Allow Save to DB (true)
+            console.log(`05-${taskNumber + 1} - inserted leaderboard for week ${number} [${ result ? 'success' : 'fail' }]`);
+
+            // Manage task threads using task timer
+            await taskTimer(timerSetting * 1000); // Await timer in ms
+          }
+
+          // Get leaderboards, verify weeks count is equal
+          console.log('06 - get leaderboards to verify');
+          const leaderboardsAfter = LeaderboardsCollection.find({}).fetch();
+          console.log('07 - found leaderboards', leaderboardsAfter.length);
+
+          if (leaderboardsAfter.length === weeks.length) {
+            console.log('08 - migration successful\n\n');
+            return true;
+          } else {
+            console.log('06 - operation failed, run down migration\n\n');
+            // Failed migration, run down migration
+            return this.v_01_refreshLeaderboards_down();
+          }
         }
 
+        // Determine the total number of tasks to perform
+        const taskTotal = weeks.length;
+
+        console.log('04 - starting task queue processing')
+        performTasks(taskTotal);
       }
       // Existing leaderboards detected
       else {
@@ -75,7 +90,7 @@ class MigrationService {
       console.log('MIGRATION', 'v_01_refreshLeaderboards_down');
 
       // Get leaderboards
-
+      console.log('TODO - Clean up migration tasks');
       // If empty, do nothing
       
       // Empty the collection
